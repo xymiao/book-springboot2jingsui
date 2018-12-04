b7ad93749f159bea117819d6fc4e5451a2c7ea6e
xymiao <xymiaotec@126.com>
2018-12-04 11:35:21 +0800

    springboot2 的基础框架搭建

---
+++ S0101-springboot2-hello/src/main/java/com/jkyuntu/springboot2/HelloSpringBootApplication.java
@@ -0,0 +1,21 @@
+package com.jkyuntu.springboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelloSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringBootApplication.class, args);
+	}
+
+	@RequestMapping("hello")
+	public String helloSpringBoot() {
+		String  sayHi = "Hello Spring Boot !!!";
+		return sayHi;
+	}
+}
